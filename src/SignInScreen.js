96e98143e725df489cb45fdca3eb7adf151b2415
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-05-10 02:17:48 -0400

    changing presentation for poc

--- src/SignInScreen.js
+++ src/SignInScreen.js
@@ -36,7 +36,8 @@ const uiConfig = {
 function SignInScreen() {
     return (
         <div>
-            <h1>My App</h1>
+            <h1>Project and Portfolio V:<br /> Aimee Ramirez</h1>
+
             <p>Please sign-in:</p>
             <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
         </div>
