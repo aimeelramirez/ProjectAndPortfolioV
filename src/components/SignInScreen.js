8535aeb7f3ba5f531a8a1928d15811b3b96360e5
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-05-16 02:58:07 -0400

    capital to lowercase

--- src/components/SignInScreen.js
+++ src/components/SignInScreen.js
@@ -5,7 +5,7 @@ import firebase from 'firebase';
 import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css'
 // import * as actionTypes from "../action/action";
-import Navigation from './Navigation'
+import Navigation from './navigation'
 import { useHistory } from 'react-router-dom'
 // Configure Firebase.
 
