8298e92cb9a1052a75353bc55341d2df7c408484
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-05-22 05:01:31 -0400

    unsplash

--- src/action/unsplash.js
+++ src/action/unsplash.js
@@ -1,10 +1,9 @@
 
-let apiKey = 'nBAYk2vkWhWn_frzSCYPHRU5bwFjHjSCrsCDEEyJcbA'
 export const FetchProductImages = async () => {
     return await fetch('https://api.unsplash.com/search/photos?page=2&query=product?orientation=squareish', {
         method: 'GET',
         headers: {
-            'Authorization': 'Client-ID ' + apiKey,
+            'Authorization': 'Client-ID ' + process.env.REACT_APP_UNSPLASH_APIKEY,
             'Accept': 'application/json',
             'Cache-Control': 'no-cache'
         }
