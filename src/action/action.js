601a0643dcfa9f32a20bc1c3a60c281be57db968
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-05-25 01:03:21 -0400

    Updates

--- src/action/action.js
+++
@@ -1,9 +0,0 @@
-export const ADD_POST = "ADD_POST";
-export const UPDATE_POST = "UPDATE_POST";
-export const REMOVE_POST = "REMOVE_POST";
-export const ADD_QUOTE = "ADD_POST";
-export const REMOVE_QUOTE = "REMOVE_POST";
-export const UPDATE_QUOTE = "UPDATE_POST";
-
-export const LOGIN_USER = "LOGIN_USER";
-export const LOGOUT_USER = "LOGOUT_USER";
