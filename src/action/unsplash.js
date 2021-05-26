b919e84bfd7197ffce0188cc483839599c5b40c8
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2021-05-26 02:08:28 -0400

    api on query

--- src/action/unsplash.js
+++ src/action/unsplash.js
@@ -1,6 +1,6 @@
 
 export const FetchProductImages = async () => {
-    return await fetch('https://api.unsplash.com/search/photos?page=1&query=product', {
+    return await fetch('https://api.unsplash.com/search/photos?page=1&query=code', {
         method: 'GET',
         headers: {
             'Authorization': 'Client-ID ' + process.env.REACT_APP_UNSPLASH_APIKEY,
@@ -9,17 +9,7 @@ export const FetchProductImages = async () => {
         }
     }).then((response) => response.json());
 };
-// export const FetchProductImages1 = async () => {
-//     return await fetch('https://api.unsplash.com/search/photos?page=3&query=product', {
-//         method: 'GET',
-//         headers: {
-//             'Authorization': 'Client-ID ' + process.env.REACT_APP_UNSPLASH_APIKEY,
-//             'Accept': 'application/json',
-//             'Cache-Control': 'no-cache'
-//         }
-//     }).then((response) => response.json());
-// };
-// export const FetchProductImages2 = async () => {
+// export const FetchProductImagesRow = async () => {
 //     return await fetch('https://api.unsplash.com/search/photos?page=2&query=product', {
 //         method: 'GET',
 //         headers: {
